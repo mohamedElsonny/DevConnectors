1a284171037ef3d74c0b4882511c56571fb3fc88
Mohamed Gamal <mohamedelsonny5@gmail.com>
2018-06-19 11:22:57 +0200

    remove service worker

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
