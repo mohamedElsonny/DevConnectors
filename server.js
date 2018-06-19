d49436e53fa1ede4f81e74a23532030c827557bb
Mohamed Gamal <mohamedelsonny5@gmail.com>
2018-06-19 11:05:00 +0200

    edit heroku-postbuild

--- server.js
+++ server.js
@@ -34,10 +34,11 @@ app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
 
-// serve static assets if in production
-if (process.env.NODE_ENV) {
-	// set a static folder
-	app.use(express.static('./client/build'));
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
