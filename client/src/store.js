ed4514bf643c4ace130f9a1da875aed8f167a965
Mohamed Gamal <mohamedelsonny5@gmail.com>
2018-06-21 03:17:49 +0200

    fix redux devtool

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middlewares = [thunk];
 const initialState = {};
@@ -8,10 +9,7 @@ const initialState = {};
 const store = createStore(
 	rootReducer,
 	initialState,
-	compose(
-		applyMiddleware(...middlewares),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 export default store;
